1388cee137dbb8c2c2950ccc254aad3c93902f0e
Yan Qi <34167870+yanqi8@users.noreply.github.com>
2018-04-24 21:32:49 -0400

    Add files via upload

---
+++ VectorCapsNet-v0/settings.py
@@ -0,0 +1,34 @@
+"""A script for matrix capsule with EM routing, settings for train/tests."""
+
+
+
+
+
+import capsules
+
+import tensorflow as tf
+
+
+slim = tf.contrib.slim
+
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string(
+  'data_dir', 'data/mnist', 'Data Directory'
+)
+tf.app.flags.DEFINE_string(
+  'train_dir', 'log/train', 'Train Directory.'
+)
+tf.app.flags.DEFINE_string(
+  'tests_dir', 'log/tests', 'Tests Directory.'
+)
+tf.app.flags.DEFINE_string(
+  'checkpoint_path', FLAGS.train_dir,
+  'The directory where the model was written to or an absolute path to a checkpoint file.'
+)
+
+tf.app.flags.DEFINE_integer(
+  'batch_size', 24, 'Train/Tests Batch Size.'
+)
+
